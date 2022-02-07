9314db090bc271aa23333f1b70a12e08b439e823
shaileshkumar <856shailesh@gmail.com>
2022-02-07 08:30:28 +0530

    easy palindrom 125 , must see

---
+++ LeetCode/Strings/easy/ValidPalindrome125.java
@@ -0,0 +1,29 @@
+package rememberStringPrograms.LeetCode.Strings.easy;
+
+public class ValidPalindrome125 {
+
+    public static boolean isPalindrome(String s) {
+        if(s.isEmpty()) return true;
+        int head = 0 ; int tail = s.length()-1;
+        char cHead , cTail;
+        while(head <= tail){
+            cHead=s.charAt(head);
+            cTail=s.charAt(tail);
+            if(!Character.isLetterOrDigit(cHead)) head++;
+            else if(!Character.isLetterOrDigit(cTail)) tail--;
+            else {
+                if(Character.toLowerCase(cHead)!= Character.toLowerCase(cTail)){
+                    return false;
+                }
+                head++; tail--;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String word = "A man, a plan, a canal: Panama";
+        boolean a = isPalindrome(word);
+        System.out.println(a);
+    }
+}
